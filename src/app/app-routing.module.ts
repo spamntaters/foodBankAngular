b760ba85924c2dbc779ae33f24fcecf49d579aab
Samuel Murray <smurra28@uncc.edu>
2019-10-09 20:40:04 -0400

    changed routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DonationListComponent } from './donation-list/donation-list.component';
+import { DonationListComponent } from './donations/donation-list/donation-list.component';
 import { InventoryListComponent } from './inventory-list/inventory-list.component';
+
 
 
 const routes: Routes = [
   {path: 'donations', component: DonationListComponent},
-  {path: 'inventory', component: InventoryListComponent}
+  {path: '', component: InventoryListComponent}
 ];
 
 @NgModule({
