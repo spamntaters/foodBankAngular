e1b4a868fb49592f35a74748f6688e5500f58b22
Samuel Murray <smurra28@uncc.edu>
2019-10-18 13:48:10 -0400

    Add queries to donations

--- src/app/donations/donation-list/donation-list.component.ts
+++ src/app/donations/donation-list/donation-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Donation } from '../../models/donation.model'
 import { DonationsService } from '../donations.service';
 import { Observable, Subscription } from 'rxjs';
+import { FormBuilder, Validators, FormGroup, FormControl } from "@angular/forms";
 // import styles from 'donation-list.scss';
 
 @Component({
@@ -14,11 +15,23 @@ export class DonationListComponent implements OnInit {
   selectorSub: Subscription;
   filters: boolean = false;
   numCol: Number = 2;
+  queryForm: FormGroup;
 
-  constructor(private donationsService: DonationsService) { }
+  constructor(private donationsService: DonationsService, private fb: FormBuilder) { }
 
   ngOnInit() {
     this.donations = this.donationsService.getAllDonations();
+
+    this.queryForm = this.fb.group({
+      queryType: null,
+      fromDate: null,
+      toDate: null,
+      donorName: '',
+      minWeight: null,
+      maxWeight: null
+    });
+
+
   }
 
   ngOnDestroy() {
@@ -35,7 +48,25 @@ export class DonationListComponent implements OnInit {
   }
 
   toggleFilters(){
+    console.log(this.queryForm.get("queryType").value)
     this.filters = !this.filters;
+  }
+
+  processQuery(){
+    let queryType: String = this.queryForm.get('queryType').value;
+    let res: Observable<Donation[]>;
+    switch(queryType){
+      case 'date':
+        res = this.donationsService.getDonationsByDate(this.queryForm.get('fromDate').value, this.queryForm.get('toDate').value) as Observable<Donation[]>;
+        break;
+      case 'weight':
+        res = this.donationsService.getDonationsByWeight(this.queryForm.get('minWeight').value, this.queryForm.get('maxWeight').value) as Observable<Donation[]>;
+        break;
+      case 'donor':
+        res = this.donationsService.getDonationsByDonor(this.queryForm.get('donorName').value) as Observable<Donation[]>;
+        break;
+    }
+    this.donations = res;
   }
 
 }
