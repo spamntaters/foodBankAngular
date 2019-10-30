85eb6ac4f5591108e31fe4150759dab59aee7f8a
Samuel Murray <smurra28@uncc.edu>
2019-10-30 12:33:26 -0400

    Work on transaction form

--- src/app/donations/donations.service.ts
+++ src/app/donations/donations.service.ts
@@ -1,47 +1,52 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { Donation } from '../models/donation.model';
-
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject, Observable } from "rxjs";
+import { Donation } from "../models/donation.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class DonationsService {
-
   selectedDonation = new BehaviorSubject<Donation>(null);
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  apiURL = 'https://jamil-niner-foodpantry-api.herokuapp.com';
+  apiURL = "https://jamil-niner-foodpantry-api.herokuapp.com";
 
-  selectDonation(donation: Donation){
+  selectDonation(donation: Donation) {
     this.selectedDonation.next(donation);
   }
-  getSelectedDonation(){
+  getSelectedDonation() {
     return this.selectedDonation.asObservable();
   }
 
   getAllDonations() {
     return this.http.get(`${this.apiURL}/donations`) as Observable<Donation[]>;
   }
-  
-  getDonation(id: Number){
+
+  getDonation(id: Number) {
     return this.http.get(`${this.apiURL}/donations?donationId=${id}`);
   }
 
-  getDonationsByDate(fromDate: String, toDate: String){
-    return this.http.get(`${this.apiURL}/donations?fromDate=${fromDate}&toDate=${toDate}`);
+  getDonationsByDate(fromDate: String, toDate: String) {
+    return this.http.get(
+      `${this.apiURL}/donations?fromDate=${fromDate}&toDate=${toDate}`
+    );
   }
 
-  getDonationsByWeight(minWeight: Number, maxWeight: Number){
-    return this.http.get(`${this.apiURL}/donations?minWeight=${minWeight}&maxWeight=${maxWeight}`);
+  getDonationsByWeight(minWeight: Number, maxWeight: Number) {
+    return this.http.get(
+      `${this.apiURL}/donations?minWeight=${minWeight}&maxWeight=${maxWeight}`
+    );
   }
 
-  getDonationsByDonor(donorName: String){
+  getDonationsByDonor(donorName: String) {
     return this.http.get(`${this.apiURL}/donations?donorName=${donorName}`);
   }
 
-  addDonation(donation: Donation){
+  addDonation(donation: Donation) {
     return this.http.post(`${this.apiURL}/donations`, donation);
+  }
+  addTransaction(transaction) {
+    return this.http.delete(`${this.apiURL}/inventory`, transaction);
   }
 }
