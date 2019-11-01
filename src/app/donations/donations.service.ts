3f54b9184d8d9803cd9ec22a16e5b716ef78ab13
Samuel Murray <smurra28@uncc.edu>
2019-11-01 00:25:34 -0400

    Switch donations back to heroku

--- src/app/donations/donations.service.ts
+++ src/app/donations/donations.service.ts
@@ -11,8 +11,7 @@ export class DonationsService {
   selectedDonation = new BehaviorSubject<Donation>(null);
   constructor(private http: HttpClient) {}
 
-  // apiURL = "https://jamil-niner-foodpantry-api.herokuapp.com";
-  apiURL = "http://localhost:8080"
+  apiURL = "https://jamil-niner-foodpantry-api.herokuapp.com";
 
   selectDonation(donation: Donation) {
     this.selectedDonation.next(donation);
