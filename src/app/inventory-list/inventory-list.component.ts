17f2f8fe164f93f8245499bfcecc7f7a1301449e
Chris Malang <cmalang@uncc.edu>
2019-11-18 16:59:36 -0500

    fuzzySearch

--- src/app/inventory-list/inventory-list.component.ts
+++ src/app/inventory-list/inventory-list.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { InventoryItem } from '../models/inventory-item.model';
-import { InventoryListService } from '../inventory-list.service';
+import { Component, OnInit } from "@angular/core";
+import { InventoryItem } from "../models/inventory-item.model";
+import { InventoryListService } from "../inventory-list.service";
+import { registerLocaleData } from "@angular/common";
 
 @Component({
-  selector: 'app-inventory-list',
-  templateUrl: './inventory-list.component.html',
-  styleUrls: ['./inventory-list.component.scss']
+  selector: "app-inventory-list",
+  templateUrl: "./inventory-list.component.html",
+  styleUrls: ["./inventory-list.component.scss"]
 })
 export class InventoryListComponent implements OnInit {
   inventorys: InventoryItem[];
@@ -14,30 +15,46 @@ export class InventoryListComponent implements OnInit {
   constructor(private inventoryService: InventoryListService) { }
 
   ngOnInit() {
-    this.inventoryService.getAllInventorys()
+    this.inventoryService
+      .getAllInventorys()
       .subscribe((data: InventoryItem[]) => {
         this.inventorys = data;
       });
     console.log(`Inventorys: ${this.inventorys}`);
   }
 
-  queryByName = ($event) => {
+  queryByName = $event => {
     $event.preventDefault();
-    if (this.nameQueryTerm == '') {
-      this.inventoryService.getAllInventorys()
+    if (this.nameQueryTerm == "") {
+      this.inventoryService
+        .getAllInventorys()
         .subscribe((data: InventoryItem[]) => {
           this.inventorys = data;
         });
-      return
+      return;
     }
 
     let list = [];
-    this.inventoryService.getInventoryByName(this.nameQueryTerm)
+    this.inventoryService
+      .getInventoryByName(this.nameQueryTerm)
       .subscribe((data: InventoryItem[]) => {
         list.push(data);
         this.inventorys = list;
       });
+  };
 
-  }
-
+  fuzzySearch = ($event) => {
+    let list;
+    this.inventoryService
+      .getAllInventorys()
+      .subscribe((data: InventoryItem[]) => {
+        list = data;
+        let regex =
+          ($event.target.value === "") ? new RegExp(".*") : new RegExp(`\^${$event.target.value}`, "i");
+        this.inventorys = list.filter((x: InventoryItem) => {
+          console.log(x.foodItemName);
+          return regex.test(x.foodItemName);
+        });
+      });
+  };
 }
