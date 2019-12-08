0e6b9596e0aee280137ebaf2a1c58a7eee690d35
Samuel Murray <samuel@induro.io>
2019-12-08 15:10:39 -0500

    Flesh out stats page

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-statistics',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./statistics.component.scss']
 })
 export class StatisticsComponent implements OnInit {
-
-  constructor() { }
+  dateForm: FormGroup;
+  statsObj$: Observable<Object>;
+  constructor(private fb: FormBuilder, http: HttpClient) {}
 
   ngOnInit() {
+    this.dateForm = this.fb.group({
+      fromDate: '',
+      toDate: ''
+    });
   }
+  get formValue() {
+    return this.dateForm.value;
+  }
+  fetchStats(){
+      //Make Call to backend passing formValue;
+      // this.statsObj$ =  this.http.get("");
+  }
+
+
 
 }
