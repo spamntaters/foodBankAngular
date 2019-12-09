c31cf16fd031d968599ea8d118e857b53e48c9e1
Samuel Murray <smurra28@uncc.edu>
2019-12-08 19:21:48 -0500

    Flesh out stats page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { InventoryListComponent } from "./inventory-list/inventory-list.componen
 import { DonationsComponent } from "./donations/donations.component";
 import { CreateDonationComponent } from "./donations/create-donation/create-donation.component";
 import { TransactionFormComponent } from "./transaction-form/transaction-form.component";
+import { StatisticsComponent } from './statistics/statistics.component';
 
 const routes: Routes = [
   { path: "donations", component: DonationsComponent },
   { path: "", component: InventoryListComponent },
   { path: "donations/new", component: CreateDonationComponent },
-  { path: "transactions/new", component: TransactionFormComponent }
+  { path: "transactions/new", component: TransactionFormComponent },
+  {path: "statistics", component: StatisticsComponent}
 ];
 
 @NgModule({
